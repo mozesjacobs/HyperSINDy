ead09ed1e66de8e11cee2a8e80cdbe8e65f3d026
mozesj <mozesj@cs.washington.edu>
2022-12-12 11:29:08 -0800

    ran some experiments

--- cmd_line.py
+++ cmd_line.py
@@ -4,8 +4,8 @@ import argparse
 # These are hyperparameters that get logged into tensorboard
 def parse_hyperparams():
     parser = argparse.ArgumentParser(description="Template")
-    parser.add_argument('-B', '--beta', default=25.0, type=float, help="KL divergence weight in loss (only for HyperSINDy)")
-    parser.add_argument('-WD', '--weight_decay', default=1e-2, type=float, help="Weight decay for sindy coefficients (only for SINDy)")
+    parser.add_argument('-B', '--beta', default=1.0, type=float, help="KL divergence weight in loss (only for HyperSINDy)")
+    parser.add_argument('-WD', '--weight_decay', default=1e-3, type=float, help="Weight decay for sindy coefficients (only for SINDy)")
     parser.add_argument('-T', '--threshold', default=0.1, type=float, help="Threshold to 0 out coefficients less than")
     parser.add_argument('-TI', '--threshold_interval', default=100, type=float, help="Epoch interval to threshold sindy coefs during training")
     parser.add_argument('-ND', '--noise_dim', default=25, type=int, help="Noise vector dimension for HyperSINDy")
@@ -30,11 +30,11 @@ def parse_args():
 
     # saving specifics
     parser.add_argument('-sess', '--session_name', default='1', type=str, help="Appended to last part of file names")
-    parser.add_argument('-DAT', '--date', default="12-02-22", type=str, help="The date"),
+    parser.add_argument('-DAT', '--date', default="12-12-22", type=str, help="The date"),
     parser.add_argument('-M',  '--model', default="HyperSINDy", type=str, help="Model to use")
     parser.add_argument('-DS', '--dataset', default="lorenz", type=str, help="Which dataset to use (lorenz)")
-    parser.add_argument('-NOISET', '--noise_type', default='x', type=str, help='Type of state-dependent noise (x, sinz)')
-    parser.add_argument('-NOISES', '--noise_scale', default=2.5, type=float, help='Scale of noise in data. Review data folder.')
+    parser.add_argument('-NOISET', '--noise_type', default='after', type=str, help='Type of state-dependent noise (x, sinz)')
+    parser.add_argument('-NOISES', '--noise_scale', default=100.0, type=float, help='Scale of noise in data. Review data folder.')
 
     # sindy parameters
     parser.add_argument('-Z', '--z_dim', default=3, type=int, help="Size of latent vector")
