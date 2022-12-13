dda4e4d7dcf70acdaae77d05e697dddd4965b6ec
mozesj <mozesj@cs.washington.edu>
2022-12-12 23:53:20 -0800

    experimentation

--- cmd_line.py
+++ cmd_line.py
@@ -4,19 +4,20 @@ import argparse
 # These are hyperparameters that get logged into tensorboard
 def parse_hyperparams():
     parser = argparse.ArgumentParser(description="Template")
-    parser.add_argument('-B', '--beta', default=10.0, type=float, help="KL divergence weight in loss (only for HyperSINDy)")
+    parser.add_argument('-B', '--beta', default=1.0, type=float, help="KL divergence weight in loss (only for HyperSINDy)")
     parser.add_argument('-WD', '--weight_decay', default=1e-3, type=float, help="Weight decay for sindy coefficients (only for SINDy)")
     parser.add_argument('-T', '--threshold', default=0.1, type=float, help="Threshold to 0 out coefficients less than")
     parser.add_argument('-TI', '--threshold_interval', default=100, type=float, help="Epoch interval to threshold sindy coefs during training")
     parser.add_argument('-ND', '--noise_dim', default=25, type=int, help="Noise vector dimension for HyperSINDy")
     parser.add_argument('-HD', '--hidden_dim', default=64, type=str, help="Dimension of hidden layers hypernet")
-    parser.add_argument('-E', '--epochs', default=501, type=float, help="Number of epochs to train for")
+    parser.add_argument('-E', '--epochs', default=1001, type=float, help="Number of epochs to train for")
     parser.add_argument('-LR', '--learning_rate', default=1e-3, type=float, help="Learning rate")
     parser.add_argument('-BS', '--batch_size', default=250, type=float, help="Batch size")
     parser.add_argument('-AR', '--adam_reg', default=0, type=float, help="Regularization to use in ADAM optimizer")
     parser.add_argument('-C', '--clip', default=1.0, type=float, help="Gradient clipping value during training (None for no clipping)")
     return parser.parse_args()
 
+#lorenz/state-after_scale-5.0/HyperSINDy/12-07-22/2/B-1.0_WD-0.001_T-0.1_TI-100_ND-25_HD-64_E-1001_LR-0.001_BS-250_AR-0_C-1.0/train
 
 def parse_args():
     
@@ -29,12 +30,12 @@ def parse_args():
     parser.add_argument('-TB', '--tensorboard_folder', default='./runs/alternate_threshold_2/', type=str, help="Output folder for tensorboard")
 
     # saving specifics
-    parser.add_argument('-sess', '--session_name', default='meeting', type=str, help="Appended to last part of file names")
-    parser.add_argument('-DAT', '--date', default="12-12-22", type=str, help="The date"),
+    parser.add_argument('-sess', '--session_name', default='2', type=str, help="Appended to last part of file names")
+    parser.add_argument('-DAT', '--date', default="12-07-22", type=str, help="The date"),
     parser.add_argument('-M',  '--model', default="HyperSINDy", type=str, help="Model to use")
     parser.add_argument('-DS', '--dataset', default="lorenz", type=str, help="Which dataset to use (lorenz)")
-    parser.add_argument('-NOISET', '--noise_type', default='x', type=str, help='Type of state-dependent noise (x, sinz)')
-    parser.add_argument('-NOISES', '--noise_scale', default=0.0, type=float, help='Scale of noise in data. Review data folder.')
+    parser.add_argument('-NOISET', '--noise_type', default='after', type=str, help='Type of state-dependent noise (x, sinz)')
+    parser.add_argument('-NOISES', '--noise_scale', default=5.0, type=float, help='Scale of noise in data. Review data folder.')
 
     # sindy parameters
     parser.add_argument('-Z', '--z_dim', default=3, type=int, help="Size of latent vector")
@@ -61,7 +62,7 @@ def parse_args():
     parser.add_argument('-D', '--device', default=3, type=int, help='Which GPU to use')
     parser.add_argument('-LCP', '--load_cp', default=0, type=int, help='If 1, loads the model from the checkpoint. If 0, does not')
     parser.add_argument('-PF', '--print_folder', default=1, type=int, help='Iff true, prints the folder for different logs')
-    parser.add_argument('-DT', '--dt', default=0.01, type=float, help='Time change in dataset')
+    parser.add_argument('-DT', '--dt', default=0.005, type=float, help='Time change in dataset')
     parser.add_argument('-SBS', '--statistic_batch_size', default=500, type=str, help="Default batch size to sample")
     
     return parser.parse_args()
